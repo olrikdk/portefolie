aac90d37ebab65eaa758c1ecfba3e748f25978ad
Cecilie Olrik <cecilie@cecilies-mbp-2.home>
2019-01-05 12:04:37 +0100

    indholdslutlidtjava

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
 window.addEventListener("load", sidenVises);
 
-var slideIndex = 1;
-showDivs(slideIndex);
+document.querySelector("roedeprik6").classList.add("falling");
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+function prik6Forsvind(){document.querySelector("roedeprik6").classList.add("paused");
+document.querySelector("roedeprik6").classList.add("dissapear");
+
 }
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  x[slideIndex-1].style.display = "block";
-}
 
+
+
+
+document.querySelector("menu").classList.add("shake");
